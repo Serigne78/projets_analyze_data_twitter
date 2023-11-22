3149efc4724dab2300be388524850eb685af1dc0
Serigne78 <114218586+Serigne78@users.noreply.github.com>
2023-11-22 09:03:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,131 @@ import json
 import re
 import pandas as pd
 import textblob
+import matplotlib.pyplot as plt
+import random
 
-df = pd.read_json("aitweets.json")
 
 
+with open("aitweets.json", "r", encoding="utf-8") as f:
+    tweets = json.load(f)
+
+auteur = ["TwitMaster23",
+    "Tweetophile",
+    "CoeurDeTweeter",
+    "ChroniqueurTweets",
+    "FlècheDeTweet",
+    "Pionnel Pessi",
+    "Leize.py",
+    "ÉchoTweeter",
+    "Dimitri.lasdeg",
+    "PixelTweeter",
+    "Yanis180",
+    "NuageDeMots",
+    "RésonanceTwitter",
+    "HarmonieDeTweets",
+    "ÉclatDePlume",
+    "TempêteDeMots",
+    "FusionTweets",
+    "CascadeDeTweets",
+    "PoésieDePixels",
+    "ZénithTweeter",
+    "ÉclipseDeMots",
+    "AstreDeTweets",
+    "SymphonieTwitter",
+    "LueurDePlume",
+    "VirtuoseDeTweets",
+    "SillageDeMots",
+    "PépiteTweeter",
+    "OrageDeTweets",
+    "PapillonDeMots",
+    "RhapsodieTwitter",
+    "MirageDeTweets",
+    "EssenceDePlume",
+    "OndeTweeter",
+    "CascadeDeMots",
+    "BrumeDeTweets",
+    "MélopéeTwitter",
+    "ÉclatDeTweeter",
+    "EncreDeMots",
+    "FlammeDeTweets",
+    "TrésorDePlume",
+    "AuroreTweeter",
+    "MystèreDeMots",
+    "LumièreTwitter",
+    "RivièreDeTweets",
+    "VagueDePlume",
+    "ÉtoileTweeter",
+    "ÉnigmeDeTweets",
+    "BrisDeMots",
+    "InspirationTwitter",
+    "SoleilDeTweets",
+    "LabyrintheDePlume",
+    "RefletTweeter",
+    "OasisDeMots",
+    "ÉphémèreTwitter",
+    "CristalDeTweets",
+    "PassionDePlume",
+    "RafaleTweeter",
+    "CharmeDeMots",
+    "ÉclipsTwitter",
+    "CâlinDeTweets",
+    "ÉmeraudeDePlume",
+    "ÉchoTweeter",
+    "AileDeMots",
+    "LibelluleTwitter",
+    "CaresseDeTweets",
+    "SérénitéDePlume",
+    "ÉtoileFilanteTweeter",
+    "SongeDeMots",
+    "ÉveilTwitter",
+    "GazouillisDeTweets",
+    "DouceurDePlume",
+    "TempêteTweeter",
+    "ÉvasionDeMots",
+    "LueurTwitter",
+    "ArcEnCielDeTweets",
+    "InspirationDePlume",
+    "RafaleTwitter",
+    "OdysséeDeMots",
+    "ÉclatTwitter",
+    "GoutteDeTweets",
+    "MerveilleDePlume",
+    "OrageTwitter",
+    "RêveDeMots",
+    "HorizonDeTweets",
+    "VibrationDePlume",
+    "BriseTwitter",
+    "MélodieDeTweets",
+    "SérénadeDePlume",
+    "CascadeTwitter",
+    "ÉlixirDeMots",
+    "RécitDeTweeter",
+    "AubeTwitter",
+    "EssenceDePlume",
+    "ÉchoDeMots",
+    "SouffleTweeter",
+    "LueurDePlume",
+    "ÉpisodeTwitter",
+    "NuageDeMots",
+    "InspirationDeTweeter",
+    "VortexTwitter"
+    ]
+
+for tweet in tweets:
+    tweet["Author"] = random.choice(auteur)
+
+with open('aitweets.json', 'w', encoding='utf-8') as f:
+    json.dump(tweets, f, ensure_ascii=False, indent=2)
 
 # Assurez-vous d'avoir déjà chargé vos données dans un DataFrame, par exemple, df.
-
+df = pd.read_json("aitweets.json")
 def topk_hashtag(k):
     hashtags_freq = {}  # Dictionnaire pour stocker la fréquence des hashtags
 
     for tweet_text in df["TweetText"]:
         hashtags = re.findall(r'#\w+', tweet_text)  # Trouver tous les hashtags dans le texte
         for hashtag in hashtags:
-            hashtag = hashtag.lower()  # Convertir en minuscules pour compter de manière insensible à la casse
+            # Convertir en minuscules pour compter de manière insensible à la casse
             if hashtag in hashtags_freq:
                 hashtags_freq[hashtag] += 1
             else:
@@ -63,7 +174,13 @@ def find_user_hashtags(hashtag):
             print(hashtag)
             print(tweet)
 find_user_mention("#hdatasystems")
+     
+taille = len(df)
+print(taille)
 
+print(df)
+
+    
 
 def sentiment(id):
     s = df.loc[df['id'] == id]
